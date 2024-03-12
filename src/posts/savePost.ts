49291fdca32250a9b5af76cbb6a250b911369dc1
E. Hannah <echannah631@gmail.com>
2024-03-12 08:21:55 -0500

    creating new blog post using fetch

---
+++ src/posts/savePost.ts
@@ -0,0 +1,23 @@
+import { NewPostData, SavedPostData } from './types';
+
+export async function savePost(newPostData: NewPostData) {
+  const response = await fetch(process.env.REACT_APP_API_URL!, {
+    method: 'POST',
+    body: JSON.stringify(newPostData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const body = (await response.json()) as unknown;
+  assetIsSavedPost(body);
+  return { ...newPostData, ...body };
+}
+
+function assetIsSavedPost(post: any): asserts post is SavedPostData {
+  if (!('uid' in post)) {
+    throw new Error("post doesn't contain uid");
+  }
+  if (typeof post.uid !== 'number') {
+    throw new Error('uid is not a number');
+  }
+}
